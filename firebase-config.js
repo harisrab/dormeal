e80f1e00ea0c183736052545be4bb4de1df2ceca
Haris Rashid <62747193+harisrab@users.noreply.github.com>
2022-02-26 00:05:51 +0800

    Fixed Firebase Config API

--- firebase-config.js
+++ firebase-config.js
@@ -7,14 +7,13 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-	// apiKey: process.env.FIREBASE_API_KEY,
-	apiKey: "AIzaSyBPMYwZyqqLG1vLLJxLsw80AwygID5Q_VY",
-	authDomain: "dormeal-new.firebaseapp.com",
-	projectId: "dormeal-new",
-	storageBucket: "dormeal-new.appspot.com",
-	messagingSenderId: "449761113620",
-	appId: "1:449761113620:web:9d4cb29832abca5fd75901",
-	measurementId: "G-V2X0Q6NVLQ",
+	apiKey: "AIzaSyD6zJrdNqG59wzBiv66UuGnkUI9rVEpr7A",
+	authDomain: "dormeal-3.firebaseapp.com",
+	projectId: "dormeal-3",
+	storageBucket: "dormeal-3.appspot.com",
+	messagingSenderId: "387557553917",
+	appId: "1:387557553917:web:ae5df0067f0bcccae67094",
+	measurementId: "G-G0Z9NGLK3C",
 };
 
 let app;
@@ -22,8 +21,7 @@ let app;
 // Initialize Firebase
 if (getApps().length !== 0) {
 	app = getApp();
-	
-}else {
+} else {
 	app = initializeApp(firebaseConfig);
 }
 
