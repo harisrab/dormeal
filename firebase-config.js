31819a2851ccd79d07bb022b77938c322bf561c4
Haris Rashid <62747193+harisrab@users.noreply.github.com>
2022-02-25 23:52:06 +0800

    Added API on Vercel

--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApps } from "firebase/app";
+
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -17,6 +18,10 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
+if (!getApps.length){
+	
+}
+
 const app = initializeApp(firebaseConfig);
 
 export default app;
