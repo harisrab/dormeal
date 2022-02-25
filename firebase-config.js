6f65469bc0291683a451451c33abcfc0d055b101
Haris Rashid <62747193+harisrab@users.noreply.github.com>
2022-02-25 23:58:14 +0800

    Fixed App Error

--- firebase-config.js
+++ firebase-config.js
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp, getApps } from "firebase/app";
-
+import { initializeApp, getApps, getApp } from "firebase/app";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -8,20 +7,24 @@ import { initializeApp, getApps } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-	apiKey: process.env.FIREBASE_API_KEY,
+	// apiKey: process.env.FIREBASE_API_KEY,
+	apiKey: "AIzaSyBPMYwZyqqLG1vLLJxLsw80AwygID5Q_VY",
 	authDomain: "dormeal-new.firebaseapp.com",
-  	projectId: "dormeal-new",
-  	storageBucket: "dormeal-new.appspot.com",
-  	messagingSenderId: "449761113620",
-  	appId: "1:449761113620:web:9d4cb29832abca5fd75901",
-  	measurementId: "G-V2X0Q6NVLQ"
+	projectId: "dormeal-new",
+	storageBucket: "dormeal-new.appspot.com",
+	messagingSenderId: "449761113620",
+	appId: "1:449761113620:web:9d4cb29832abca5fd75901",
+	measurementId: "G-V2X0Q6NVLQ",
 };
 
-// Initialize Firebase
-if (!getApps.length){
-	
-}
+let app;
 
-const app = initializeApp(firebaseConfig);
+// Initialize Firebase
+if (getApps().length !== 0) {
+	app = getApp();
+	
+}else {
+	app = initializeApp(firebaseConfig);
+}
 
 export default app;
