d782479c222bd5e193247f03d4e05e2cf70c00bb
Haris Rashid <62747193+harisrab@users.noreply.github.com>
2022-02-28 12:25:34 +0800

    Changes

--- components/MenuItem.js
+++ components/MenuItem.js
@@ -9,14 +9,30 @@ function MenuItem({ name, price, description }) {
 
 	// cs
 
-	return (
+	return selected ? (
 		<div
 			onClick={() => setSelected(!selected)}
 			className={`card my-3 max-w-md w-full shadow-${
 				selected ? "xl" : "md"
-			} rounded-xl border-darkBrown border-[1px] bg-${
-				selected ? "darkBrown" : "lightBrown"
-			} duration-150 text-${selected ? "lightBrown" : "darkBrown"}`}
+			} rounded-xl border-darkBrown border-[1px] bg-darkBrown duration-150 text-lightBrown`}
+		>
+			<div className="flex px-5 my-3 justify-between text-[15px] font-roboto font-semibold">
+				<p className="">{name}</p>
+				<p className="">{price}</p>
+			</div>
+
+			{description && (
+				<div className="px-5 py-3 justify-between text-[13px] font-roboto font-normal w-[60%]">
+					<p>{description}</p>
+				</div>
+			)}
+		</div>
+	) : (
+		<div
+			onClick={() => setSelected(!selected)}
+			className={`card my-3 max-w-md w-full shadow-${
+				selected ? "xl" : "md"
+			} rounded-xl border-darkBrown border-[1px] bg-lightBrown duration-150 text-darkBrown`}
 		>
 			<div className="flex px-5 my-3 justify-between text-[15px] font-roboto font-semibold">
 				<p className="">{name}</p>
